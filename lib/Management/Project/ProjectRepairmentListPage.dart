4be7e9e06c4e60e182a577a8f992074457e90d0c
AlbusSong <strawberry1234561@gmail.com>
2019-12-31 00:13:03 +0200

    ProjectRepairmentListPage

--- lib/Management/Project/ProjectRepairmentListPage.dart
+++ lib/Management/Project/ProjectRepairmentListPage.dart
@@ -55,10 +55,136 @@ class _ProjectRepairmentListPageState extends State<ProjectRepairmentListPage> {
   }
 
   Widget _buildListView() {
-    return ListView(
-      physics: const AlwaysScrollableScrollPhysics(),
-      children: <Widget>[
-      ],
+    return ListView.builder(
+        // itemCount: listLength(this.arrOfData),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          // In our case, a DogCard for each doggo.
+          return GestureDetector(
+            onTap: () => _hasSelectedIndex(index),
+            child: _buildListItem(index),
+          );
+        });
+  }
+
+  Widget _buildListItem(int index) {
+    return Container(
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                    height: 25,
+                    color: Colors.white,
+                    child: Text(
+                          "返修工单：D20180404000006",
+                          maxLines: 2,
+                          style: TextStyle(
+                              color: hexColor(MAIN_COLOR_BLACK),
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold),
+                        ),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    margin: EdgeInsets.only(left: 10),
+                    height: 21,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("LotNo：13251001",
+                            style: TextStyle(
+                                color: hexColor("999999"), fontSize: 15)),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    margin: EdgeInsets.only(left: 10),
+                    height: 21,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("生产工单：20180404",
+                            style: TextStyle(
+                                color: hexColor("999999"), fontSize: 15))
+                      ],
+                    ),
+                  ),                  
+                  Container(
+                    color: Colors.white,
+                    margin: EdgeInsets.only(left: 10),
+                    height: 21,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("机型：20180404",
+                            style: TextStyle(
+                                color: hexColor("999999"), fontSize: 15))
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    margin: EdgeInsets.only(left: 10),
+                    height: 21,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("创建时间：20180404",
+                            style: TextStyle(
+                                color: hexColor("999999"), fontSize: 15))
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                    height: 21,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("数量：82",
+                            style: TextStyle(
+                                color: hexColor("999999"), fontSize: 15)),
+                        Text("返修代码：20",
+                            style: TextStyle(
+                                color: hexColor("999999"), fontSize: 15)),
+                        SizedBox(width: 10,),
+                      ],
+                    ),
+                  ),                  
+                  Container(
+                    color: hexColor("dddddd"),
+                    height: 1,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(width: 5),
+          Container(
+            margin: EdgeInsets.only(right: 8),
+            color: Colors.white,
+            child: Icon(
+              Icons.arrow_forward_ios,
+              color: hexColor("dddddd"),
+              size: 20,
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _hasSelectedIndex(int index) {
+
   }
 }
