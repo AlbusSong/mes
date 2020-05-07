1a413870b9f5a15edf95ac94bb3b2ecedbcf4ba7
AlbusSong <strawberry1234561@gmail.com>
2020-05-07 08:58:39 +0300

    二维码扫描，截取前16位
    以C开头

--- lib/Others/Tool/BarcodeScanTool.dart
+++ lib/Others/Tool/BarcodeScanTool.dart
@@ -1,15 +1,19 @@
 import 'dart:io';
 // import 'package:qrscan/qrscan.dart' as scanner;
+import 'package:mes/Others/Tool/GlobalTool.dart';
 import 'package:mes/Others/Tool/NativeCommunicationTool.dart';
 
 class BarcodeScanTool {
   static Future <String> tryToScanBarcode() async {
+    String barcodeScanRes;
     if(Platform.isIOS){
-      String barcodeScanRes = await NativeCommunicationTool().tryToScanBarcode();
-      return barcodeScanRes;
+      barcodeScanRes = await NativeCommunicationTool().tryToScanBarcode();      
     } else if (Platform.isAndroid) {
-      String barcodeScanRes = await NativeCommunicationTool().tryToScanBarcode();
-      return barcodeScanRes;
+      barcodeScanRes = await NativeCommunicationTool().tryToScanBarcode();      
     }
+    if (isAvailable(barcodeScanRes) && barcodeScanRes.startsWith("C") && barcodeScanRes.length > 16) {
+      barcodeScanRes = barcodeScanRes.substring(0, 16);
+    }
+    return barcodeScanRes;
   }
 }
